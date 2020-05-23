f50b854e6bd71e031ab9f05f3888d2155d941082
Dinozaur_G <karnah@mail.ru>
2020-05-23 22:36:17 +0700

    sadfgh

--- NOrmal/Form1.Designer.cs
+++ NOrmal/Form1.Designer.cs
@@ -29,12 +29,309 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea4 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend4 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series10 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series11 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series12 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.labelForTime3 = new System.Windows.Forms.Label();
+            this.labelForTime2 = new System.Windows.Forms.Label();
+            this.labelForChi3 = new System.Windows.Forms.Label();
+            this.labelForD3 = new System.Windows.Forms.Label();
+            this.labelForE3 = new System.Windows.Forms.Label();
+            this.labelForTime1 = new System.Windows.Forms.Label();
+            this.labelForChi2 = new System.Windows.Forms.Label();
+            this.labelForD2 = new System.Windows.Forms.Label();
+            this.labelForE2 = new System.Windows.Forms.Label();
+            this.labelForChi1 = new System.Windows.Forms.Label();
+            this.labelForD1 = new System.Windows.Forms.Label();
+            this.labelForE1 = new System.Windows.Forms.Label();
+            this.buttonAnalysis = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.NNval = new System.Windows.Forms.NumericUpDown();
+            this.nVal = new System.Windows.Forms.NumericUpDown();
+            this.chartForAnalysis = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.NNval)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nVal)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chartForAnalysis)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.labelForTime3);
+            this.panel1.Controls.Add(this.labelForTime2);
+            this.panel1.Controls.Add(this.labelForChi3);
+            this.panel1.Controls.Add(this.labelForD3);
+            this.panel1.Controls.Add(this.labelForE3);
+            this.panel1.Controls.Add(this.labelForTime1);
+            this.panel1.Controls.Add(this.labelForChi2);
+            this.panel1.Controls.Add(this.labelForD2);
+            this.panel1.Controls.Add(this.labelForE2);
+            this.panel1.Controls.Add(this.labelForChi1);
+            this.panel1.Controls.Add(this.labelForD1);
+            this.panel1.Controls.Add(this.labelForE1);
+            this.panel1.Controls.Add(this.buttonAnalysis);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.NNval);
+            this.panel1.Controls.Add(this.nVal);
+            this.panel1.Location = new System.Drawing.Point(13, 13);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1768, 100);
+            this.panel1.TabIndex = 0;
+            // 
+            // labelForTime3
+            // 
+            this.labelForTime3.AutoSize = true;
+            this.labelForTime3.Location = new System.Drawing.Point(1579, 75);
+            this.labelForTime3.Name = "labelForTime3";
+            this.labelForTime3.Size = new System.Drawing.Size(0, 17);
+            this.labelForTime3.TabIndex = 16;
+            // 
+            // labelForTime2
+            // 
+            this.labelForTime2.AutoSize = true;
+            this.labelForTime2.Location = new System.Drawing.Point(1579, 42);
+            this.labelForTime2.Name = "labelForTime2";
+            this.labelForTime2.Size = new System.Drawing.Size(0, 17);
+            this.labelForTime2.TabIndex = 15;
+            // 
+            // labelForChi3
+            // 
+            this.labelForChi3.AutoSize = true;
+            this.labelForChi3.Location = new System.Drawing.Point(1330, 75);
+            this.labelForChi3.Name = "labelForChi3";
+            this.labelForChi3.Size = new System.Drawing.Size(0, 17);
+            this.labelForChi3.TabIndex = 14;
+            // 
+            // labelForD3
+            // 
+            this.labelForD3.AutoSize = true;
+            this.labelForD3.Location = new System.Drawing.Point(1330, 42);
+            this.labelForD3.Name = "labelForD3";
+            this.labelForD3.Size = new System.Drawing.Size(0, 17);
+            this.labelForD3.TabIndex = 13;
+            // 
+            // labelForE3
+            // 
+            this.labelForE3.AutoSize = true;
+            this.labelForE3.Location = new System.Drawing.Point(1330, 8);
+            this.labelForE3.Name = "labelForE3";
+            this.labelForE3.Size = new System.Drawing.Size(0, 17);
+            this.labelForE3.TabIndex = 12;
+            // 
+            // labelForTime1
+            // 
+            this.labelForTime1.AutoSize = true;
+            this.labelForTime1.Location = new System.Drawing.Point(1579, 8);
+            this.labelForTime1.Name = "labelForTime1";
+            this.labelForTime1.Size = new System.Drawing.Size(0, 17);
+            this.labelForTime1.TabIndex = 11;
+            // 
+            // labelForChi2
+            // 
+            this.labelForChi2.AutoSize = true;
+            this.labelForChi2.Location = new System.Drawing.Point(981, 77);
+            this.labelForChi2.Name = "labelForChi2";
+            this.labelForChi2.Size = new System.Drawing.Size(0, 17);
+            this.labelForChi2.TabIndex = 10;
+            // 
+            // labelForD2
+            // 
+            this.labelForD2.AutoSize = true;
+            this.labelForD2.Location = new System.Drawing.Point(981, 42);
+            this.labelForD2.Name = "labelForD2";
+            this.labelForD2.Size = new System.Drawing.Size(0, 17);
+            this.labelForD2.TabIndex = 9;
+            // 
+            // labelForE2
+            // 
+            this.labelForE2.AutoSize = true;
+            this.labelForE2.Location = new System.Drawing.Point(981, 8);
+            this.labelForE2.Name = "labelForE2";
+            this.labelForE2.Size = new System.Drawing.Size(0, 17);
+            this.labelForE2.TabIndex = 8;
+            // 
+            // labelForChi1
+            // 
+            this.labelForChi1.AutoSize = true;
+            this.labelForChi1.Location = new System.Drawing.Point(503, 77);
+            this.labelForChi1.Name = "labelForChi1";
+            this.labelForChi1.Size = new System.Drawing.Size(0, 17);
+            this.labelForChi1.TabIndex = 7;
+            // 
+            // labelForD1
+            // 
+            this.labelForD1.AutoSize = true;
+            this.labelForD1.Location = new System.Drawing.Point(503, 42);
+            this.labelForD1.Name = "labelForD1";
+            this.labelForD1.Size = new System.Drawing.Size(0, 17);
+            this.labelForD1.TabIndex = 6;
+            // 
+            // labelForE1
+            // 
+            this.labelForE1.AutoSize = true;
+            this.labelForE1.Location = new System.Drawing.Point(503, 4);
+            this.labelForE1.Name = "labelForE1";
+            this.labelForE1.Size = new System.Drawing.Size(0, 17);
+            this.labelForE1.TabIndex = 5;
+            // 
+            // buttonAnalysis
+            // 
+            this.buttonAnalysis.Location = new System.Drawing.Point(231, 3);
+            this.buttonAnalysis.Name = "buttonAnalysis";
+            this.buttonAnalysis.Size = new System.Drawing.Size(148, 94);
+            this.buttonAnalysis.TabIndex = 4;
+            this.buttonAnalysis.Text = "Ковальски, анализ!";
+            this.buttonAnalysis.UseVisualStyleBackColor = true;
+            this.buttonAnalysis.Click += new System.EventHandler(this.buttonAnalysis_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(65, 75);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(18, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "N";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(62, 4);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(16, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "n";
+            // 
+            // NNval
+            // 
+            this.NNval.Location = new System.Drawing.Point(104, 75);
+            this.NNval.Maximum = new decimal(new int[] {
+            100000000,
+            0,
+            0,
+            0});
+            this.NNval.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.NNval.Name = "NNval";
+            this.NNval.Size = new System.Drawing.Size(120, 22);
+            this.NNval.TabIndex = 1;
+            this.NNval.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // nVal
+            // 
+            this.nVal.Location = new System.Drawing.Point(104, 3);
+            this.nVal.Maximum = new decimal(new int[] {
+            100000000,
+            0,
+            0,
+            0});
+            this.nVal.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.nVal.Name = "nVal";
+            this.nVal.Size = new System.Drawing.Size(120, 22);
+            this.nVal.TabIndex = 0;
+            this.nVal.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // chartForAnalysis
+            // 
+            chartArea4.Name = "ChartArea1";
+            this.chartForAnalysis.ChartAreas.Add(chartArea4);
+            legend4.Name = "Legend1";
+            this.chartForAnalysis.Legends.Add(legend4);
+            this.chartForAnalysis.Location = new System.Drawing.Point(13, 120);
+            this.chartForAnalysis.Name = "chartForAnalysis";
+            series10.ChartArea = "ChartArea1";
+            series10.Legend = "Legend1";
+            series10.Name = "Series1";
+            series11.ChartArea = "ChartArea1";
+            series11.Legend = "Legend1";
+            series11.Name = "Series2";
+            series12.ChartArea = "ChartArea1";
+            series12.Legend = "Legend1";
+            series12.Name = "Series3";
+            this.chartForAnalysis.Series.Add(series10);
+            this.chartForAnalysis.Series.Add(series11);
+            this.chartForAnalysis.Series.Add(series12);
+            this.chartForAnalysis.Size = new System.Drawing.Size(1768, 300);
+            this.chartForAnalysis.TabIndex = 1;
+            this.chartForAnalysis.Text = "chart1";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick_1);
+            // 
+            // timer2
+            // 
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick_1);
+            // 
+            // timer3
+            // 
+            this.timer3.Tick += new System.EventHandler(this.timer3_Tick_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1793, 450);
+            this.Controls.Add(this.chartForAnalysis);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.NNval)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nVal)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chartForAnalysis)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button buttonAnalysis;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown NNval;
+        private System.Windows.Forms.NumericUpDown nVal;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chartForAnalysis;
+        private System.Windows.Forms.Label labelForChi2;
+        private System.Windows.Forms.Label labelForD2;
+        private System.Windows.Forms.Label labelForE2;
+        private System.Windows.Forms.Label labelForChi1;
+        private System.Windows.Forms.Label labelForD1;
+        private System.Windows.Forms.Label labelForE1;
+        private System.Windows.Forms.Label labelForTime3;
+        private System.Windows.Forms.Label labelForTime2;
+        private System.Windows.Forms.Label labelForChi3;
+        private System.Windows.Forms.Label labelForD3;
+        private System.Windows.Forms.Label labelForE3;
+        private System.Windows.Forms.Label labelForTime1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Timer timer3;
     }
 }
 
--- NOrmal/Form1.cs
+++ NOrmal/Form1.cs
@@ -12,9 +12,212 @@ namespace NOrmal
 {
     public partial class Form1 : Form
     {
+        DateTime date1 = new DateTime(0, 0);
+        DateTime date2 = new DateTime(0, 0);
+        DateTime date3 = new DateTime(0, 0);
         public Form1()
         {
             InitializeComponent();
+        }
+        private void buttonAnalysis_Click(object sender, EventArgs e)
+        {
+            chartForAnalysis.Series[0].Points.Clear();
+            chartForAnalysis.Series[1].Points.Clear();
+            chartForAnalysis.Series[2].Points.Clear();
+            Random rnd = new Random();
+            int i;
+            int n = (int)nVal.Value;
+            int N = (int)NNval.Value;
+            int m = (int)(Math.Log(N) + 1);
+            int[] numsInInter1 = new int[m];
+            int[] numsInInter2 = new int[m];
+            int[] numsInInter3 = new int[m];
+            for (i = 0; i < m; i++)
+            {
+                numsInInter1[i] = 0;
+                numsInInter2[i] = 0;
+                numsInInter3[i] = 0;
+            }
+            double[] random1 = new double[N];
+            double[] random2 = new double[N];
+            double[] random3 = new double[N];
+            double sum = 0, E1 = 0, E2 = 0, E3 = 0, D1 = 0, D2 = 0, D3 = 0, Chi1 = 0, Chi2 = 0, Chi3 = 0;
+            double max1 = -1000000000, min1 = 1000000000, max2 = -1000000000, min2 = 1000000000, max3 = -1000000000, min3 = 1000000000, MIN, MAX;
+            double inter;
+            timer1.Start();
+            for (i = 0; i < N; i++)
+            {
+                sum = 0;
+                for (int j = 0; j < n; j++)
+                {
+                    sum += rnd.NextDouble() % 1;
+                }
+                random1[i] = Math.Sqrt(12 / n) * (sum - n / 2);
+                E1 += random1[i];
+                if (random1[i] <= min1)
+                    min1 = random1[i];
+                if (random1[i] >= max1)
+                    max1 = random1[i];
+            }
+            timer1.Stop();
+            timer2.Start();
+            for (i = 0; i < N; i++)
+            {
+                sum = 0;
+                for (int j = 0; j < n; j++)
+                {
+                    sum += rnd.NextDouble() % 1;
+                }
+                double tmp = Math.Sqrt(12 / n) * (sum - n / 2);
+                random2[i] = tmp + (1 / 240 * (Math.Pow(tmp,3) - 3 * tmp));
+                E2 += random2[i];
+                if (random2[i] <= min2)
+                    min2 = random2[i];
+                if (random2[i] >= max2)
+                    max2 = random2[i];
+            }
+            timer2.Stop();
+            timer3.Start();
+            for (i = 0; i < N; i += 2)
+            {
+                double b1 = rnd.NextDouble() % 1;
+                double b2 = rnd.NextDouble() % 1;
+                random3[i] = Math.Sqrt(-2 * Math.Log(b1)) * Math.Cos(2 * Math.PI * b2);
+                random3[i + 1] = Math.Sqrt(-2 * Math.Log(b1)) * Math.Sin(2 * Math.PI * b2);
+                E3 += random3[i];
+                E3 += random3[i + 1];
+                if (random3[i] <= min3)
+                    min3 = random3[i];
+                if (random3[i] >= max3)
+                    max3 = random3[i];
+                if (random3[i + 1] <= min3)
+                    min3 = random3[i + 1];
+                if (random3[i + 1] >= max3)
+                    max3 = random3[i + 1];
+            }
+            timer3.Stop();
+            E1 *= (1 / N);
+            E2 *= (1 / N);
+            E3 *= (1 / N);
+            D1 *= (1 / N);
+            D2 *= (1 / N);
+            D3 *= (1 / N);
+            D1 -= Math.Pow(E1, 2);
+            D2 -= Math.Pow(E2, 2);
+            D3 -= Math.Pow(E3, 2);
+            labelForE1.Text = "Average = " + (Double)E1;
+            labelForE2.Text = "Average = " + (Double)E2;
+            labelForE3.Text = "Average = " + (Double)E3;
+            labelForD1.Text = "Variance = " + (Double)D1;
+            labelForD2.Text = "Variance = " + (Double)D2;
+            labelForD3.Text = "Variance = " + (Double)D3;
+            if (min1 < min2)
+            {
+                if (min1 < min3)
+                    MIN = min1;
+                else
+                    MIN = min3;
+            }
+            else
+            {
+                if (min2 < min3)
+                    MIN = min2;
+                else
+                    MIN = min3;
+            }
+            if (max1 > max2)
+            {
+                if (max1 > max3)
+                    MAX = max1;
+                else
+                    MAX = max3;
+            }
+            else
+            {
+                if (max2 > max3)
+                    MAX = max2;
+                else
+                    MAX = max3;
+            }
+            inter = (Math.Abs(MIN) + Math.Abs(MAX)) / m;
+            for (i = 0; i < N; i++)
+            {
+                for(int j = 0; j < m - 1; j++)
+                {
+                    if (random1[i] > (MIN + inter * j) && random1[i] < (MIN + (inter * (j + 1))))
+                    {
+                        numsInInter1[j]++;
+                        break;
+                    }
+                }
+            }
+            for (i = 0; i < N; i++)
+            {
+                for (int j = 0; j < m - 1; j++)
+                {
+                    if (random2[i] > (MIN + inter * j) && random2[i] < (MIN + (inter * (j + 1))))
+                    {
+                        numsInInter2[j]++;
+                        break;
+                    }
+                }
+            }
+            for (i = 0; i < N; i++)
+            {
+                for (int j = 0; j < m - 1; j++)
+                {
+                    if (random3[i] > (MIN + inter * j) && random3[i] < (MIN + (inter * (j + 1))))
+                    {
+                        numsInInter3[j]++;
+                        break;
+                    }
+                }
+            }
+            for (i = 0; i < m; i++)
+            {
+                Chi1 += Math.Pow(numsInInter1[i], 2) / (N * (numsInInter1[i] / N));
+            }
+            for (i = 0; i < m; i++)
+            {
+                Chi2 += Math.Pow(numsInInter2[i], 2) / (N * (numsInInter2[i] / N));
+            }
+            for (i = 0; i < m; i++)
+            {
+                Chi3 += Math.Pow(numsInInter3[i], 2) / (N * (numsInInter3[i] / N));
+            }
+            Chi1 -= N;
+            Chi2 -= N;
+            Chi3 -= N;
+            labelForChi1.Text = "Chi-squared = " + (Double)Chi1;
+            labelForChi2.Text = "Chi-squared = " + (Double)Chi2;
+            labelForChi3.Text = "Chi-squared = " + (Double)Chi3;
+            for (i = 0; i < m; i++)
+            {
+                chartForAnalysis.Series[0].Points.AddXY((((MIN + inter * i) + (inter * (i + 1))) / 2), (numsInInter1[i] / m));
+            }
+            for (i = 0; i < m; i++)
+            {
+                chartForAnalysis.Series[1].Points.AddXY((((MIN + inter * i) + (inter * (i + 1))) / 2), (numsInInter2[i] / m));
+            }
+            for (i = 0; i < m; i++)
+            {
+                chartForAnalysis.Series[2].Points.AddXY((((MIN + inter * i) + (inter * (i + 1))) / 2), (numsInInter3[i] / m));
+            }
+        }
+        private void timer1_Tick_1(object sender, EventArgs e)
+        {
+            date1 = date1.AddSeconds(1);
+            labelForTime1.Text = date1.ToString("mm:ss");
+        }
+        private void timer2_Tick_1(object sender, EventArgs e)
+        {
+            date2 = date2.AddSeconds(1);
+            labelForTime2.Text = date2.ToString("mm:ss");
+        }
+        private void timer3_Tick_1(object sender, EventArgs e)
+        {
+            date3 = date3.AddSeconds(1);
+            labelForTime3.Text = date3.ToString("mm:ss");
         }
     }
 }
